0a0220ad134b2563a67cfc37238da82546768e42
Duc Trieu <ductrieuuet@gmail.com>
2023-03-09 15:14:55 +0700

    add queue feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebVitalsModule } from './web-vitals/web-vitals.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
     WebVitalsModule,
     MongooseModule.forRoot('mongodb://localhost/web-vitals'),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
